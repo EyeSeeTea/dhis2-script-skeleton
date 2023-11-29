0b68df3029e8eb89d9cc2c36e3ae5856b0d8d79e
Arnau Sanchez <pyarnau@gmail.com>
2023-11-29 10:30:22 +0100

    Add indexById

--- src/domain/entities/Base.ts
+++ src/domain/entities/Base.ts
@@ -1,3 +1,5 @@
+import _ from "./generic/Collection";
+import { HashMap } from "./generic/HashMap";
 export type Id = string;
 export type Ref = { id: Id };
 export type NamedRef = { id: Id; name: string };
@@ -9,6 +11,10 @@ export type IndexedById<T> = Record<Id, T>;
 
 export function getId<Obj extends Ref>(obj: Obj): Id {
     return obj.id;
+}
+
+export function indexById<T extends Ref>(objs: T[]): HashMap<Id, T> {
+    return _(objs).keyBy(getId);
 }
 
 export type Code = string;
