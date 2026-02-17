222f32509e5ada27ed4e33110887fe26467a3bd2
xurxodev <xurxosf@gmail.com>
2026-02-17 09:49:22 +0100

    Fix dependency rule violation
    - Avoid infrastructure dependency directly from a logger that write in console

--- src/domain/usecases/GetCurrentUserUseCase.ts
+++ src/domain/usecases/GetCurrentUserUseCase.ts
@@ -2,15 +2,15 @@ import { Async } from "domain/entities/Async";
 import { User } from "domain/entities/User";
 import { UserRepository } from "domain/repositories/UserRepository";
 
-import logger from "utils/log";
+import { Logger } from "../logger/Logger";
 
 export class GetCurrentUserUseCase {
-    constructor(private userRepository: UserRepository) {}
+    constructor(private logger: Logger, private userRepository: UserRepository) {}
 
     async execute(): Async<User> {
-        logger.debug("Fetching user information...");
+        this.logger.debug("Fetching user information...");
         const currentUser = await this.userRepository.getCurrent();
-        logger.info(`Current User: id=${currentUser.id}, name=${currentUser.name}`);
+        this.logger.info(`Current User: id=${currentUser.id}, name=${currentUser.name}`);
         return currentUser;
     }
 }
