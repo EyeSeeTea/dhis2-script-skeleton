e473c728a676099062ed20f27a5040ce67e89fef
xurxodev <xurxosf@gmail.com>
2026-02-17 10:18:05 +0100

    Use Future instead of Promises

--- src/domain/repositories/NotificationsRepository.ts
+++ src/domain/repositories/NotificationsRepository.ts
@@ -1,5 +1,6 @@
 import { Notification } from "domain/entities/Notification";
+import { FutureData } from "domain/entities/generic/Future";
 
 export interface NotificationsRepository {
-    send(notification: Notification): Promise<void>;
+    send(notification: Notification): FutureData<void>;
 }
--- src/domain/usecases/GetCurrentUserUseCase.ts
+++ src/domain/usecases/GetCurrentUserUseCase.ts
@@ -1,16 +1,16 @@
-import { Async } from "domain/entities/Async";
 import { User } from "domain/entities/User";
+import { FutureData } from "domain/entities/generic/Future";
+import { Logger } from "domain/logger/Logger";
 import { UserRepository } from "domain/repositories/UserRepository";
-
-import { Logger } from "../logger/Logger";
 
 export class GetCurrentUserUseCase {
     constructor(private logger: Logger, private userRepository: UserRepository) {}
 
-    async execute(): Async<User> {
+    execute(): FutureData<User> {
         this.logger.debug("Fetching user information...");
-        const currentUser = await this.userRepository.getCurrent();
-        this.logger.info(`Current User: id=${currentUser.id}, name=${currentUser.name}`);
-        return currentUser;
+        return this.userRepository.getCurrent().map(currentUser => {
+            this.logger.info(`Current User: id=${currentUser.id}, name=${currentUser.name}`);
+            return currentUser;
+        });
     }
 }
